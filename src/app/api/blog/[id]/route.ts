43dd4d54b8aded0bed75daa79481ec7e2145747e
likuor <likuor0528@gmail.com>
2023-11-14 11:04:53 -0800

    add: edit a post of API

--- src/app/api/blog/[id]/route.ts
+++ src/app/api/blog/[id]/route.ts
@@ -26,3 +26,34 @@ export const GET = async (req: Request, res: NextResponse) => {
     prisma.$disconnect;
   }
 };
+
+export const PUT = async (req: Request, res: NextResponse) => {
+  try {
+    await connectPrisma();
+    const id: number = parseInt(req.url.split('/blog/')[1]);
+    const { title, description } = await req.json();
+
+    const post = await prisma.post.update({
+      data: { title, description },
+      where: { id },
+    });
+
+    return NextResponse.json(
+      {
+        message: 'Success to update a post',
+        data: post,
+      },
+      { status: 200 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      {
+        message: 'Error',
+        data: error,
+      },
+      { status: 500 }
+    );
+  } finally {
+    prisma.$disconnect;
+  }
+};
