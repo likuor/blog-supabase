03b415bfd34b2ca75326bff11330a2e82fe23b0e
likuor <likuor0528@gmail.com>
2023-11-14 12:25:18 -0800

    add: adite page

---
+++ src/app/blog/edit/[id]/page.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const EditPost = () => {
+  return (
+    <>
+      <div className="w-full m-auto flex my-4">
+        <div className="flex flex-col justify-center items-center m-auto">
+          <p className="text-2xl text-slate-200 font-bold p-3">ブログの編集 🚀</p>
+          <form>
+            <input
+              placeholder="タイトルを入力"
+              type="text"
+              className="rounded-md px-4 w-full py-2 my-2"
+            />
+            <textarea
+              placeholder="記事詳細を入力"
+              className="rounded-md px-4 py-2 w-full my-2"
+            ></textarea>
+            <button className="font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100">
+              更新
+            </button>
+            <button className="ml-2 font-semibold px-4 py-2 shadow-xl bg-red-400 rounded-lg m-auto hover:bg-slate-100">
+              削除
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default EditPost;
