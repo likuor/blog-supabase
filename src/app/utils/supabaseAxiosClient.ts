43895e2bae0ceb917837d976bd9a43a3b2f5a66d
likuor <likuor0528@gmail.com>
2023-11-21 12:53:52 -0800

    add: fix get all

--- src/app/utils/supabaseAxiosClient.ts
+++ src/app/utils/supabaseAxiosClient.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 // const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseUrl = 'https://blog-supabase-five-dev.vercel.app/api/blog';
 // DEV env
 // const supabaseUrlDev = process.env.NEXT_PUBLIC_SUPABASE_URL_DEV;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
