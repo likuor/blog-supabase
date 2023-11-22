23ace721e7711f18abdbc609e2f14ce51d46789e
likuor <likuor0528@gmail.com>
2023-11-21 16:39:11 -0800

    add: text fditing

--- src/app/blog/add/page.tsx
+++ src/app/blog/add/page.tsx
@@ -14,21 +14,21 @@ const AddPost = () => {
       <Toaster />
       <div className="w-full m-auto flex my-4">
         <div className="flex flex-col justify-center items-center m-auto">
-          <p className="text-2xl text-slate-200 font-bold p-3">ブログ新規作成 🚀</p>
+          <p className="text-2xl text-slate-200 font-bold p-3">Add a new blog 🚀</p>
           <form onSubmit={handleSubmit}>
             <input
               ref={titleRef}
-              placeholder="タイトルを入力"
+              placeholder="Type title"
               type="text"
               className="rounded-md px-4 w-full py-2 my-2"
             />
             <textarea
               ref={descriptionRef}
-              placeholder="記事詳細を入力"
+              placeholder="Type content"
               className="rounded-md px-4 py-2 w-full my-2"
             ></textarea>
             <button className="font-semibold px-4 py-2 shadow-xl bg-slate-200 rounded-lg m-auto hover:bg-slate-100">
-              投稿
+              Post
             </button>
           </form>
         </div>
