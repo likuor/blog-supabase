2053f97b19facca8fa5c76f5850dec6f0903e435
likuor <likuor0528@gmail.com>
2023-11-21 16:10:26 -0800

    add: fix get all to fetch

--- src/app/controller.ts
+++ src/app/controller.ts
@@ -2,14 +2,14 @@ import { supabaseAxiosClient } from './utils/supabaseAxiosClient';
 
 const HomeController = () => {
   const fetchAllPosts = async () => {
-    // const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL!, {
-    //   cache: 'no-store',
-    // });
+    const res = await fetch(process.env.NEXT_PUBLIC_BASE_URL!, {
+      cache: 'no-store',
+    });
 
-    // const data = await res.json();
-    // return data.data;
-    const res = await supabaseAxiosClient.get('');
-    return res.data;
+    const data = await res.json();
+    return data.data;
+    // const res = await supabaseAxiosClient.get('');
+    // return res.data;
   };
 
   return { fetchAllPosts };
